411ae22bfa2ff7fe368d481375818ecc8ab0a8ff
xuhui <good122000@qq.com>
2019-10-14 15:55:05 +0800

    @dev # 支持UIView

--- ios/RNSScreenStack.m
+++ ios/RNSScreenStack.m
@@ -68,9 +68,15 @@
 {
   RNSScreenView *screen;
   if (operation == UINavigationControllerOperationPush) {
+    if(![toVC.view isKindOfClass:RNSScreenView.class]) {
+      return nil	       
+    }
     screen = (RNSScreenView *) toVC.view;
   } else if (operation == UINavigationControllerOperationPop) {
-   screen = (RNSScreenView *) fromVC.view;
+    if(![fromVC.view isKindOfClass:RNSScreenView.class]) {
+      return nil;
+    }
+    screen = (RNSScreenView *) fromVC.view;
   }
   if (screen != nil && screen.stackAnimation != RNSScreenStackAnimationDefault) {
     return  [[RNSScreenStackAnimator alloc] initWithOperation:operation];
